7d5c1fab13a12a14c3f3291874d165f2e9d321c1
IvanShihTw <152954995+IvanShihTw@users.noreply.github.com>
2024-05-16 19:40:56 +0800

    Create generate_sitemap.py

---
+++ generate_sitemap.py
@@ -0,0 +1,20 @@
+import os
+
+def generate_sitemap(directory):
+    sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                file_path = os.path.relpath(os.path.join(root, file), directory)
+                url = 'https://yourusername.github.io/repositoryname/' + file_path
+                sitemap += f'  <url>\n    <loc>{url}</loc>\n  </url>\n'
+
+    sitemap += '</urlset>\n'
+
+    with open('sitemap.xml', 'w') as f:
+        f.write(sitemap)
+
+if __name__ == '__main__':
+    generate_sitemap('IvanShihTw/ivanshihtw.github.io')
